9441f67d5e833f1d28fbebf389c5fa3d9fd184d7
B-Vritant <vritantb7@gmail.com>
2024-10-16 03:38:03 +0530

    Add files via upload
    Added all required files for the website

---
+++ script.js
@@ -0,0 +1,21 @@
+let moodleVisible = false;
+
+var burger = document.getElementById("burger");
+var moodle = document.getElementById("moodle");
+var cross = document.getElementById("cross");
+
+burger.addEventListener("click", () => {
+    if (moodleVisible === false) {
+        moodle.style.visibility = "visible";
+        burger.style.visibility = "hidden";
+        moodleVisible = true;
+    }
+});
+
+cross.addEventListener("click", () => {
+    if (moodleVisible === true) {
+        moodle.style.visibility = "hidden";
+        burger.style.visibility = "visible";
+        moodleVisible = false;
+    }
+});
